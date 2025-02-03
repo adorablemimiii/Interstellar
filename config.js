372da5862775e4709c944844177a44b7c3595a8a
adorablemimiii <mimisch48@gmail.com>
2025-02-03 10:59:01 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,13 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
     interstellar: "password",
   },
 };
 
-export default config;
+export default config;$env:config=true pnpm start
+config=true bun start
+config=true npm start
+
+
